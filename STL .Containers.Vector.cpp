d0cb6329903c401e69394f63f965709c1375b00b
deep_049 <deepsarker049@gmail.com>
2025-05-20 14:03:49 +0600

    STL .Containers.Vector.cpp

--- STL .Containers.Vector.cpp
+++ STL .Containers.Vector.cpp
@@ -6,20 +6,21 @@ int main() {
     v1.push_back(5);
     v1.push_back(10);
     v1.push_back(20);
-
+// Regular loop
     for (int i=0; i<v1.size(); i++)
    printf("%d ", v1[i]) ; 
    cout<< endl ;
-
+    
+// Range based loop
    for (int i: v1)  
    printf("%d ", i);
    cout << endl;
-
+    
+// using iterator
    for (vector <int> :: iterator it = v1.begin(); it !=v1.end(); it++) 
    { 
       cout<< *it << " ";
    }  
-    
     
     return 0;
 }
